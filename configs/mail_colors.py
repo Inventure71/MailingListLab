d47ffd081ef29838c5d8633f71c5ee9f86be2fc1
inventure71 <matteo.giorgetti.05@gmail.com>
2025-03-19 12:15:10 +0100

    Add mail color configuration, news extraction settings, and email whitelist

---
+++ configs/mail_colors.py
@@ -0,0 +1,12 @@
+
+
+
+class MailColor:
+    category_colors = {
+        "News": "#007BFF",
+        "Talks": "#28A745",
+        "Events": "#DC3545",
+        "Opportunity": "#FFC107",
+        "General": "#6c757d",  # default color if no category is provided
+        "Other": "#6c757d",
+    }

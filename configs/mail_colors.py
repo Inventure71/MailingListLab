64ef7d2665c0d0a524978b8b5b20f60c0a7b3e66
inventure71 <matteo.giorgetti.05@gmail.com>
2025-03-19 13:01:00 +0100

    Add color configuration for Workshops category in mail_colors.py

--- configs/mail_colors.py
+++ configs/mail_colors.py
@@ -6,6 +6,7 @@ class MailColor:
         "News": "#007BFF",
         "Talks": "#28A745",
         "Events": "#DC3545",
+        "Workshops": "#17A2B8",
         "Opportunity": "#FFC107",
         "General": "#6c757d",  # default color if no category is provided
         "Other": "#6c757d",
